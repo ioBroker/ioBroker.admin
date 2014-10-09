809ac75513e7fb2e4d82beb59ce3d83b09517c81
GermanBluefox <dogafox@gmail.com>
2014-10-09 22:27:59 +0200

    (Bluefox) optimize Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -191,7 +191,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('default', [
         'exec',
-        //'http',
+        'http',
         'clean',
         'replace',
         'updateReadme',
