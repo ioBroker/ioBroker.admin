904f113f2d5749cd5a8b3dae4bfd0241668c4961
soef <soef@gmx.net>
2017-10-14 14:42:25 +0200

    fix sorting, when using adaptsers with more than one instance

--- www/lib/js/selectID.js
+++ www/lib/js/selectID.js
@@ -258,10 +258,19 @@
     function sortTree(data) {
         var objects = data.objects;
         var checkStatesFirst;
-        switch (data.sortConfig.statesFirst) {
+        switch (sortConfig.statesFirst) {
             case undefined: checkStatesFirst = function() { return 0 }; break;
             case true:      checkStatesFirst = function(child1, child2) { return ((~~child2.folder) - (~~child1.folder))}; break;
             case false:     checkStatesFirst = function(child1, child2) { return ((~~child1.folder) - (~~child2.folder))}; break;
+        }
+
+        function compAdapterAndInstance(c1, c2) {
+            var s1 = c1.key.substr(0, c1.key.indexOf('.')+1);
+            var s2 = c2.key.substr(0, c2.key.indexOf('.')+1);
+
+            if (s1 > s2) return 1;
+            if (s1 < s2) return -1;
+            return 0;
         }
 
         function sortByName(child1, child2) {
@@ -272,7 +281,13 @@
             if (o1 && o2) {
                 var c1 = o1.common, c2 = o2.common;
                 if (c1 && c2) {
-                    if (!data.sortConfig.ignoreSortOrder && c1.sortOrder && c2.sortOrder) {
+
+                    var s1 = c1.key.substr(0, c1.key.indexOf('.')+1); // faster than regexp.
+                    var s2 = c2.key.substr(0, c2.key.indexOf('.')+1);
+                    if (s1 > s2) return 1;
+                    if (s1 < s2) return -1;
+
+                    if (!sortConfig.ignoreSortOrder && c1.sortOrder && c2.sortOrder) {
                         if (c1.sortOrder > c2.sortOrder) return 1;
                         if (c1.sortOrder < c2.sortOrder) return -1;
                         return 0;
@@ -291,11 +306,16 @@
         function sortByKey(child1, child2) {
             var ret = checkStatesFirst(child1, child2);
             if (ret) return ret;
-            if (!data.sortConfig.ignoreSortOrder) {
+            if (!sortConfig.ignoreSortOrder) {
                 var o1 = objects[child1.key], o2 = objects[child2.key];
                 if (o1 && o2) {
                     var c1 = o1.common, c2 = o2.common;
                     if (c1 && c2 && c1.sortOrder && c2.sortOrder) {
+                        var s1 = c1.key.substr(0, c1.key.indexOf('.')+1);  // faster than regexp.
+                        var s2 = c2.key.substr(0, c2.key.indexOf('.')+1);
+                        if (s1 > s2) return 1;
+                        if (s1 < s2) return -1;
+
                         if (c1.sortOrder > c2.sortOrder) return 1;
                         if (c1.sortOrder < c2.sortOrder) return -1;
                         return 0;
@@ -306,7 +326,6 @@
             if (child1.key < child2.key) return -1;
             return 0;
         }
-
 
         data.$tree.fancytree('getRootNode').sortChildren(data.sort ? sortByName : sortByKey, true);
         //var tree = data.$tree.fancytree('getTree');
