c23f3faeafbd56ea01112dabdc456cc97f3816dd
soef <soef@gmx.net>
2017-11-06 19:47:43 +0100

    log view adapted

--- www/js/adminLog.js
+++ www/js/adminLog.js
@@ -7,49 +7,37 @@ function Logs(main) {
 
     this.logLinesCount        = 0;
     this.logLinesStart        = 0;
-    this.logHosts             = [];
-    this.logFilterTimeout     = null;
-    this.logFilterHost        = '';
-    this.logFilterSeverity    = '';
-    this.logFilterMessage     = '';
-    this.$logFilterHost       = null;
-    this.$logFilterSeverity   = null;
-    this.$logFilterMessage    = null;
-    
-    this.logPauseList         = [];
-    this.logPauseMode         = false;
-    this.logPauseOverflow     = false;
-    this.logPauseCounterSpan  = null;
-    this.logPauseCounter      = [];
-    
+
+    var $logTable, $logOuter, $logPause;
+    var logFilterHost = '', logFilterSeverity = '', logFilterMessage = '';
+    var pause = {
+        list: [],
+        mode: false,
+        counter: 0,
+        overflow: false,
+        $counterSpan: null
+    };
+
+    var hdr;
+
     this.prepare = function () {
-        that.$logFilterSeverity = $('#log-filter-severity');
-        that.$logFilterHost     = $('#log-filter-host');
-        that.$logFilterMessage  = $('#log-filter-message');
-        
-        that.$logFilterHost.change(this.filter);
-        that.$logFilterSeverity.change(this.filter);
+        $logOuter = $('#log-outer');
+        $logTable = $('#log-table');
+        $logPause = $('#log-pause');
 
-        that.$logFilterMessage.change(function () {
-            if (that.logFilterTimeout) clearTimeout(that.logFilterTimeout);
-            that.logFilterTimeout = setTimeout(that.filter, 600);
-        }).keyup(function (e) {
-            if (e.which === 13) {
-                that.filter();
-            } else {
-                $(this).trigger('change');
-            }
-        });
+        hdr = IobListHeader ('log-outer-header', { list: $logOuter, colWidthOffset: 1, prefix: 'log-filter' });
+        hdr.doFilter = that.filter;
 
-        $('#log-filter-message-clear')
-            .button({icons: {primary: 'ui-icon-close'}, text: false})
-            .css({height: 18, width: 18})
-            .click(function () {
-                var $log_filter = $('#log-filter-message');
-                if ($log_filter.val() !== '') {
-                    $log_filter.val('').trigger('change');
-                }
-            });
+        hdr.add('combobox', 'Adapter', 'host');
+        hdr.add('text', 'Time');
+        hdr.add('combobox', '', 'severity', [
+            { val: "", name: 'debug' },
+            { val: "silly", name: 'silly' },
+            { val: "info", name: 'info' },
+            { val: "warn", name: 'warn' },
+            { val: "error", name: 'error' }
+        ]);
+        hdr.add('edit', 'Message', 'message');
 
         $('#log-clear-on-disk').button({icons:{primary: 'ui-icon-trash'}, text: false}).click(function () {
             that.main.confirmMessage(_('Log file will be deleted. Are you sure?'), null, null, function (result) {
@@ -63,25 +51,26 @@ function Logs(main) {
                     });
                 }
             });
-        }).css({width: 20, height: 20}).addClass('ui-state-error');
+        })/*.css({width: 20, height: 20})*/.addClass('ui-state-error');
 
         $('#log-refresh').button({icons:{primary: 'ui-icon-refresh'}, text: false}).click(function () {
             that.clear();
-        }).css({width: 20, height: 20});
+        })/*.css({width: 20, height: 20})*/;
 
-        $('#log-pause')
+        //$('#log-pause')
+        $logPause
             .button({icons:{primary: 'ui-icon-pause'}, text: false})
-            .css({height: 20})
+            //.css({height: 20})
             .attr('title', _('Pause output'))
             .click(function () {
                 that.pause();
             });
 
-        this.logPauseCounterSpan = $('#log-pause .ui-button-text');
+        pause.$counterSpan = $logPause.find('ui-button-text');
 
-        $('#log-clear').button({icons:{primary: 'ui-icon-trash'}, text: false}).click(function () {
+        $('#log-clear').button({icons:{primary: 'ui-icon-close'}, text: false}).click(function () {
             that.clear(false);
-        }).css({width: 20, height: 20});
+        })/*.css({width: 20, height: 20});*/
 
         $('#log-copy-text').click(function () {
             $('#log-copy-text').hide().html('');
@@ -91,7 +80,7 @@ function Logs(main) {
         $('#log-copy').button({icons:{primary: 'ui-icon-copy'}, text: false}).click(function () {
             var text = '<span class="error">' + _('copy note') + '</span>';
             $('#tabs').hide();
-            $('#log-copy-text').show().html(text + '<br><table style="width: 100%; font-size:12px" id="log-copy-table">' + $('#log-table').html() + '</table>');
+            $('#log-copy-text').show().html(text + '<br><table style="width: 100%; font-size:12px" id="log-copy-table">' + $logTable.html() + '</table>');
             var lines = $('#log-copy-table .log-column-4');
             for (var t = 0; t < lines.length; t++) {
                 var q = $(lines[t]);
@@ -103,73 +92,83 @@ function Logs(main) {
 
     function installColResize() {
         if (!$.fn.colResizable) return;
-        var $outer = $('#log-outer');
-
-        if ($outer.is(':visible')) {
-            $outer.colResizable({
+        if ($logOuter.is(':visible')) {
+            $logOuter.colResizable({
                 liveDrag: true,
+
+                partialRefresh: true,
+                marginLeft: 5,
+                postbackSafe:true,
+
                 onResize: function (event) {
-                    // read width of data.$tree and set the same width for header
-                    var thDest = $('#log-outer-header >thead>tr>th');	//if table headers are specified in its semantically correct tag, are obtained
-                    var thSrc = $outer.find('>tbody>tr:first>td');
-                    for (var i = 1; i < thSrc.length; i++) {
-                        $(thDest[i]).attr('width', $(thSrc[i]).width());
-                    }
+                    return hdr.syncHeader();
+                    // // read width of data.$tree and set the same width for header
+                    // var thDest = $('#log-outer-header >thead>tr>th');	//if table headers are specified in its semantically correct tag, are obtained
+                    // var thSrc = $outer.find('>tbody>tr:first>td');
+                    // for (var i = 1; i < thSrc.length; i++) {
+                    //     $(thDest[i]).attr('width', $(thSrc[i]).width());
+                    // }
                 }
             });
+            hdr.syncHeader();
         } else {
             setTimeout(function () {
                 installColResize();
-            }, 400)
+            }, 200)
         }
     }
+
+    this.resize = function (width, height) {
+        width = width;
+    };
 
     // -------------------------------- Logs ------------------------------------------------------------
     this.init = function () {
         if (!this.main.currentHost) {
             setTimeout(function () {
                 that.init();
-            }, 500);
+            }, 100);
             return;
         }
 
-        $('#log-table').html('');
+        $logTable.html('');
         this.main.socket.emit('sendToHost', this.main.currentHost, 'getLogs', 200, function (lines) {
             setTimeout(function () {
                 var message = {message: '', severity: 'debug', from: '', ts: ''};
                 var size = lines ? lines.pop() : -1;
-                if (size !== -1) {
+                if (size != -1) {
                     size = parseInt(size);
                     $('#log-size').html((_('Log size:') + ' ' + ((size / (1024 * 1024)).toFixed(2) + ' MB ')).replace(/ /g, '&nbsp;'));
                 }
-                for (var i = 0; i < lines.length; i++) {
+                for (var i = 0, len=lines.length; i < len; i++) {
                     if (!lines[i]) continue;
+                    var line = lines[i];
                     // 2014-12-05 14:47:10.739 - info: iobroker  ERR! network In most cases you are behind a proxy or have bad network settings.npm ERR! network
-                    if (lines[i][4] === '-' && lines[i][7] === '-') {
-                        lines[i]         = lines[i].replace(/(\[[0-9]+m)/g, '');
-                        message.ts       = lines[i].substring(0, 23);
-                        lines[i]         = lines[i].substring(27);
+                    if (line[4] === '-' && line[7] === '-') {
+                        line             = line.replace(/(\[[0-9]+m)/g, '');
+                        message.ts       = line.substring(0, 23);
+                        line             = line.substring(27);
 
-                        var pos          = lines[i].indexOf(':');
-                        message.severity = lines[i].substring(0, pos);
+                        var pos          = line.indexOf(':');
+                        message.severity = line.substring(0, pos);
                         if (message.severity.charCodeAt(message.severity.length - 1) === 27) message.severity = message.severity.substring(0, message.severity.length - 1);
                         if (message.severity.charCodeAt(0) === 27) message.severity = message.severity.substring(1);
 
-                        lines[i]         = lines[i].substring(pos + 2);
-                        pos              = lines[i].indexOf(' ');
-                        message.from     = lines[i].substring(0, pos);
-                        message.message  = lines[i].substring(pos);
+                        line             = line.substring(pos + 2);
+                        pos              = line.indexOf(' ');
+                        message.from     = line.substring(0, pos);
+                        message.message  = line.substring(pos);
                     } else {
-                        message.message = lines[i];
+                        message.message = line;
                     }
                     that.add(message);
                 }
 
                 installColResize();
 
-                that.logFilterHost     = that.$logFilterHost.val();
-                that.logFilterMessage  = that.$logFilterMessage.val();
-                that.logFilterSeverity = that.$logFilterSeverity.val();
+                logFilterHost     = hdr.host.val();
+                logFilterMessage  = hdr.message.val();
+                logFilterSeverity = hdr.severity.val();
             }, 0);
         });
     };
@@ -180,19 +179,19 @@ function Logs(main) {
             message.message = message.message.substring(message.from.length + 1);
         }
 
-        if (this.logPauseMode) {
-            this.logPauseList.push(message);
-            this.logPauseCounter++;
+        if (pause.mode) {
+            pause.list.push(message);
+            pause.counter++;
 
-            if (this.logPauseCounter > this.logLimit) {
-                if (!this.logPauseOverflow) {
-                    $('#log-pause').addClass('ui-state-error')
+            if (pause.counter > this.logLimit) {
+                if (!pause.overflow) {
+                    $logPause.addClass('ui-state-error')
                         .attr('title', _('Message buffer overflow. Losing oldest'));
-                    this.logPauseOverflow = true;
+                    pause.overflow = true;
                 }
-                this.logPauseList.shift();
+                pause.list.shift();
             }
-            this.logPauseCounterSpan.html(this.logPauseCounter);
+            pause.$counterSpan.html(tdp(pause.counter));
             return;
         }
 
@@ -205,32 +204,39 @@ function Logs(main) {
             this.logLinesCount++;
         }
 
-        if (message.from && this.logHosts.indexOf(message.from) === -1) {
-            this.logHosts.push(message.from); 
-            this.logHosts.sort();
-            this.$logFilterHost.html('<option value="">' + _('all') + '</option>');
-            for (var i = 0; i < this.logHosts.length; i++) {
-                this.$logFilterHost.append('<option value="' + this.logHosts[i].replace(/\./g, '-') + '" ' + ((this.logHosts[i] === this.logFilterHost) ? 'selected' : '') + '>' + this.logHosts[i] + '</option>');
+        // if (message.from && this.logHosts.indexOf(message.from) === -1) {
+        //     this.logHosts.push(message.from);
+        //     this.logHosts.sort();
+        //     this.$logFilterHost.html('<option value="">' + _('all') + '</option>');
+        //     for (var i = 0; i < this.logHosts.length; i++) {
+        //         this.$logFilterHost.append('<option value="' + this.logHosts[i].replace(/\./g, '-') + '" ' + ((this.logHosts[i] === this.logFilterHost) ? 'selected' : '') + '>' + this.logHosts[i] + '</option>');
+        //     }
+        // }
+
+        if (message.from) {
+            hdr.host.checkAddOption(message.from, function (o) {
+                return { val: o.replace(/\./g, '-'), name: o };
+            })
             }
-        }
+
         var visible = '';
         var from    = message.from ? message.from.replace(/\./g, '-') : '';
 
         if (this.logFilterHost && this.logFilterHost !== from) visible = 'display: none';
 
-        if (!visible && this.logFilterSeverity) {
+        if (!visible && logFilterSeverity) {
             if (this.logFilterSeverity === 'debug' && message.severity === 'silly') {
                 visible = 'display: none';
             } else if (this.logFilterSeverity === 'info' && (message.severity === 'debug' || message.severity === 'silly')) {
                 visible = 'display: none';
-            } else if (this.logFilterSeverity === 'warn' && message.severity !== 'warn' && message.severity !== 'error') {
+            } else if (logFilterSeverity === 'warn' && message.severity !== 'warn' && message.severity !== 'error') {
                 visible = 'display: none';
-            } else if (this.logFilterSeverity === 'error' && message.severity !== 'error') {
+            } else if (logFilterSeverity === 'error' && message.severity !== 'error') {
                 visible = 'display: none';
             }
         }
 
-        if (!visible && this.logFilterMessage && message.message.indexOf(that.logFilterMessage) === -1) {
+        if (!visible && logFilterMessage && message.message.indexOf(logFilterMessage) === -1) {
             visible = 'display: none';
         }
 
@@ -242,28 +248,22 @@ function Logs(main) {
         text += '<td class="log-column-3">' + message.severity + '</td>';
         text += '<td class="log-column-4" title="' + message.message.replace(/"/g, "'") + '">' + message.message.substring(0, 200) + '</td></tr>';
 
-        $('#log-table').prepend(text);
+        $logTable.prepend(text);
     };
 
     this.filter = function () {
-        if (that.logFilterTimeout) {
-            clearTimeout(that.logFilterTimeout);
-            that.logFilterTimeout = null;
-        }
-        var $logOuter  = $('#log-outer');
+        logFilterHost     = hdr.host.val();
+        logFilterMessage  = hdr.message.val();
+        logFilterSeverity = hdr.severity.val();
 
-        that.logFilterHost     = that.$logFilterHost.val();
-        that.logFilterMessage  = that.$logFilterMessage.val();
-        that.logFilterSeverity = that.$logFilterSeverity.val();
-
-        if (that.logFilterSeverity === 'error') {
+        if (logFilterSeverity === 'error') {
             $logOuter.find('.log-severity-silly').hide();
             $logOuter.find('.log-severity-debug').hide();
             $logOuter.find('.log-severity-info').hide();
             $logOuter.find('.log-severity-warn').hide();
             $logOuter.find('.log-severity-error').show();
         } else
-        if (that.logFilterSeverity === 'warn') {
+        if (logFilterSeverity === 'warn') {
             $logOuter.find('.log-severity-silly').hide();
             $logOuter.find('.log-severity-debug').hide();
             $logOuter.find('.log-severity-info').hide();
@@ -277,7 +277,7 @@ function Logs(main) {
             $logOuter.find('.log-severity-warn').show();
             $logOuter.find('.log-severity-error').show();
         } else
-        if (that.logFilterSeverity === 'silly') {
+        if (logFilterSeverity === 'silly') {
             $logOuter.find('.log-severity-silly').show();
             $logOuter.find('.log-severity-debug').show();
             $logOuter.find('.log-severity-info').show();
@@ -290,12 +290,12 @@ function Logs(main) {
             $logOuter.find('.log-severity-warn').show();
             $logOuter.find('.log-severity-error').show();
         }
-        if (that.logFilterHost || that.logFilterMessage) {
+
+        if (logFilterHost || logFilterMessage) {
             $logOuter.find('.log-line').each(function () {
-                if (that.logFilterHost && !$(this).hasClass('log-from-' + that.logFilterHost)) {
+                if (logFilterHost && !$(this).hasClass('log-from-' + logFilterHost)) {
                     $(this).hide();
-                } else
-                if (that.logFilterMessage && $(this).html().indexOf(that.logFilterMessage) === -1) {
+                } else if (logFilterMessage && $(this).html().indexOf(logFilterMessage) === -1) {
                     $(this).hide();
                 }
             });
@@ -304,7 +304,7 @@ function Logs(main) {
 
     this.clear = function (isReload) {
         if (isReload === undefined) isReload = true;
-        $('#log-table').html('');
+        $logTable.html('');
         this.logLinesCount = 0;
         this.logLinesStart = 0;
         $('a[href="#tab-log"]').removeClass('errorLog');
@@ -317,28 +317,28 @@ function Logs(main) {
     };
 
     this.pause = function () {
-        if (!this.logPauseMode) {
-            var $logPause = $('#log-pause');
+        if (!pause.mode) {
             $logPause
-                .addClass('ui-state-focus')
+                .addClass('ui-state-focus ui-state-error log-pause-button-active')
                 .button('option', 'text', true)
                 .button('option', 'icons', {primary: null});
 
-            this.logPauseCounterSpan = $logPause.find('.ui-button-text');
-            this.logPauseCounterSpan.html('0').css({'padding-top': '1px', 'padding-bottom': '0px'});
-            this.logPauseCounter  = 0;
-            this.logPauseMode     = true;
+            pause.$counterSpan = $logPause.find('.ui-button-text');
+            pause.$counterSpan.addClass().html('0');
+            pause.counter  = 0;
+            pause.mode     = true;
         } else {
-            this.logPauseMode     = false;
-            for (var i = 0; i < this.logPauseList.length; i++) {
-                this.add(this.logPauseList[i]);
+            pause.mode     = false;
+            for (var i = 0; i < pause.list.length; i++) {
+                this.add(pause.list[i]);
             }
-            this.logPauseOverflow = false;
-            this.logPauseList     = [];
-            this.logPauseCounter  = 0;
+            pause.overflow = false;
+            pause.list.length = 0;
+            pause.counter  = 0;
 
             $logPause
                 .removeClass('ui-state-error ui-state-focus')
+                .removeClass('log-pause-button-active')
                 .attr('title', _('Pause output'))
                 .button('option', 'text', false).button('option', 'icons', {primary: 'ui-icon-pause'});
         }
---
+++ www/js/listHeader.js
@@ -0,0 +1,192 @@
+
+
+
+function IobListHeader (header, options) {
+    if (!(this instanceof IobListHeader)) return new IobListHeader(header, options);
+
+    if (options === undefined) options = {};
+    if (options.colWidthOffset === undefined) options.colWidthOffset = 0;
+
+    var $headerThs, $listTds, $header;
+    $header = typeof header === 'object' ? $(header) : $('#' + header);
+    if ($header[0].tagName === 'TABLE') $header = $header.find('tr:first');
+
+    var self = this;
+
+    $header.html('');
+
+    this.setList = function (_list) {
+        var $tds;
+        if (typeof _list === 'string') {
+            if (_list[0] !== '#') _list = '#' + _list;
+            $tds = $(_list)
+        } else {
+            $tds = _list;
+        }
+        if (!$tds || !$tds.length) return;
+        if ($tds[0].tagName !== 'TD' && $tds[0].tagName !== 'TH') {
+            $tds = $tds.find (">thead>tr:first>th,>thead>tr:first>td");
+            if (!$tds.length) $tds = _list.find (">tbody>tr:first>th,>tr:first>th,>tbody>tr:first>td, >tr:first>td");
+        }
+        $listTds = $tds;
+    };
+    if (options.list) this.setList(options.list);
+
+
+    this.syncHeader = function () {
+        if (typeof $listTds !== 'object') return;
+
+        this.syncHeader = function() {
+            var px = $listTds.parent().width();
+            $listTds.each (function (i, o) {
+                if (i >= $listTds.length - 1) return;
+                //$ ($headerThs[i]).css({ width: $(o).css('width')});
+                //$ ($headerThs[i]).width ($(o).width());
+                //return;
+                // var x = $ (o).width ();
+                // if (x) $ ($headerThs[i]).width ($ (o).width () + options.colWidthOffset);
+                o = $(o);
+                var x, xs = o.css('width');
+                if (xs.indexOf('%') >= 0) {
+                    x = parseInt(xs) * px / 100;
+                } else {
+                    x =$(o).width();
+                }
+                if (x) $ ($headerThs[i]).width (x + options.colWidthOffset);
+            });
+        };
+        this.syncHeader();
+    };
+
+    var resizeTimer;
+    $(window).resize(function (x, y) {
+        if (resizeTimer) clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(self.syncHeader.bind(self), 100);
+    });
+
+    function buildId (id, fis) {
+        if (!id || (fis && id[0] === '#')) return id;
+        if (options.prefix && id.substr(0, options.prefix.length) !== options.prefix) {
+            id = options.prefix + '-' + id;
+        }
+        return (fis ? '#' : '') + id;
+    }
+
+    this.doFilter = function () {};
+
+    function allOption(name, selectedVal) {
+        name = name ? _(name) + ' ('+_('all') + ')' : _('all');
+        return '<option value="" ' + ((selectedVal === "") ? 'selected' : '') + '>' + name + '</option>';
+    }
+
+    self.add = function (what, title, _id, selectOptions) {
+        var id = buildId(_id);
+        title = _(title);
+
+        var txt = '';
+        switch (what) {
+            case 'combobox':
+                txt =
+                    '<td style="width: 100%">\n' +
+                    //'    <select id="' + id + '" title="' + title + '">' + opts + '</select>' +
+                    '    <select id="' + id + '" title="' + title + '">'+'</select>' +
+                    '</td>' +
+                    '<td>' +
+                    '    <button id="' + id + '-clear" role="button" title=""></button>' +
+                    '</td>';
+                break;
+            case "edit":
+                txt =
+                    '<td style="width: 100%">' +
+                    '    <input placeholder="' + title + '" type="text" id="' + id + '" title="' + title + '">' +
+                    '</td>' +
+                    '<td>' +
+                    '    <button id="' + id + '-clear" role="button" title="' + title + '"></button>' +
+                    '</td>';
+                break;
+            case "text":
+                txt =
+                    '<td style="width: 100%"><span>' + title +
+                    '</span></td>';
+                break;
+        }
+
+        $header.append (
+            //'<td class="event-column-' + ++cnt + '">' +
+            '<th>' +
+            '<table class="main-header-input-table" style="width: 100%;">' +
+            '    <tbody>' +
+            '    <tr style="background: #ffffff; ">' +
+            txt +
+            '    </tr>' +
+            '    </tbody>' +
+            '</table>' +
+            '</th>'
+        );
+
+        var fisId = '#' + id;
+        var $id = $(fisId);
+        var elem = self[_id] = {
+            $filter: $id,
+            val: $id.val.bind($id)
+        };
+
+        if (what === 'combobox') {
+            elem.options = [];
+            elem.checkAddOption = function (text, cb, noAll) {
+                if (this.options.indexOf(text) !== -1) return;
+                this.options.push(text);
+                this.options.sort();
+                var selectedVal = $id.val();
+                var txt = noAll ? '' : allOption(title, selectedVal);
+
+                function addOption(val, name) {
+                    txt += '<option value="' + val + '" ' + ((name === selectedVal) ? 'selected' : '') + '>' + name + '</option>'
+                }
+                for (var i=0, len=this.options.length; i<len; i++) {
+                    var option = this.options[i];
+                    if (cb) {
+                        var v = cb(option, i);
+                        if (typeof v === 'object') {
+                            addOption(v.val, v.name);
+                        } else {
+                            txt += v;
+                        }
+                    } else {
+                        addOption(option, option);
+                    }
+                }
+                $id.html(txt);
+            };
+
+            if (selectOptions) {
+                for (var i = 0; i < selectOptions.length; i++) {
+                    elem.checkAddOption(selectOptions[i].name, function (o, i) {
+                        return selectOptions [i];
+                    }, true);
+                }
+            }
+        }
+
+        $(fisId + '-clear').button({icons:{primary: 'ui-icon-close'}, text: false}).click(function () {
+            if ($id.val() !== '') {
+                $id.val('').trigger('change');
+            }
+        });
+
+        var eventFilterTimeout;
+        $id.change(function () {
+            if (eventFilterTimeout) clearTimeout(eventFilterTimeout);
+            elem.selectedVal = $id.val();
+            eventFilterTimeout = setTimeout(self.doFilter, what!=='combobox' ? 400 : 0);
+        }).keyup(function (event) {
+            if (event.which === 13) {
+                self.doFilter();
+            } else {
+                $id.trigger('change');
+            }
+        });
+        $headerThs = $header.find('>th');
+    };
+}
+
