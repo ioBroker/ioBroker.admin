dc84609e470f61adafb9aa37382bb0ab8ec1f1aa
ldittmar81 <ldittmar.privat@gmail.com>
2018-01-03 12:08:08 +0100

    (ldittmar) bugtracker - small embellishments

--- src/js/adminIssue.js
+++ src/js/adminIssue.js
@@ -1,6 +1,8 @@
+/* global systemLang */
+
 function Issue(main) {
     'use strict';
-    
+
     var that = this;
     this.$dialogIssue = $('#dialog-issue');
     this.main = main;
@@ -9,7 +11,7 @@ function Issue(main) {
     };
 
     this.init = function () {
-        if (this.inited){
+        if (this.inited) {
             return;
         }
 
@@ -25,23 +27,25 @@ function Issue(main) {
             var $table = $('#result-issue');
             $table.empty();
             var count = 0;
-            $.getJSON(issue, function (data) {                
+            $.getJSON(issue, function (data) {
                 var bug = false;
                 for (var i in data) {
                     if (i === "remove") {
                         break;
                     }
-                    bug = true;
                     var issue = data[i];
-                    var $issueElement = $('#issueTable').children().clone(true, true);
-                    if(count === 0){
-                        $issueElement.find('li').addClass('active');
+                    if (issue.hasOwnProperty('pull_request')) {
+                        continue;
                     }
+                    bug = true;
+
+                    var $issueElement = $('#issueTable').children().clone(true, true);
                     $issueElement.find('.collapsible-header-title').text(issue.title);
                     $issueElement.find('.goto').attr('href', issue.html_url);
                     $issueElement.find('.user').text(issue.user.login);
                     $issueElement.find('.form-row').text(issue.body);
-                    $issueElement.find('.created').text(main.formatDate(new Date(issue.created_at), false, true));
+                    var issueDate = new Date(new Date(issue.created_at));
+                    $issueElement.find('.created').text(issueDate.toLocaleDateString(systemLang, {"weekday": "short", "year": "numeric", "month": "long", "day": "2-digit", "hour": "2-digit", "minute": "2-digit", "second": "2-digit"}));
                     if (issue.labels.length > 0) {
                         for (var k in issue.labels) {
                             if (k === "remove") {
@@ -50,30 +54,33 @@ function Issue(main) {
                             $issueElement.find('.category').append('<a class="btn-floating btn-small translateT" style="background:#' + issue.labels[k].color + ';" title="' + issue.labels[k].name + '"><span>' + issue.labels[k].name + '</span></a>');
                         }
                     }
-                    
+
                     $table.append($issueElement);
                     count++;
                 }
 
                 if (!bug) {
-                    $table.append($('<li><h2>' + _('No bug') + '</h2></li>'));
+                    $table.append($('<li><h3 class="title">' + _('No bug') + '</h3></li>'));
                 }
 
-            });
+            }).done(that.$dialogIssue.find('.collapsible').collapsible());
         }
-        
+
         that.$dialogIssue.data('name', name);
         that.$dialogIssue.find('.title').html(_('Known bugs for') + ': ' + name);
-        
-        var $collapsible = that.$dialogIssue.find('.collapsible');
-        $collapsible.collapsible();
-        
+
+        that.$dialogIssue.find('.dialog-system-buttons .btn-cancel').unbind('click').click(function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            that.main.navigate();
+        });
+
     };
 
     this.destroy = function () {
         if (this.inited) {
-            this.$dialogIssue.find('.collapsible').collapsible('destroy')
-            this.inited = false;             
+            this.$dialogIssue.find('.collapsible').collapsible('destroy');
+            this.inited = false;
         }
-    }
+    };
 }
