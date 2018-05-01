208aa8c981f7a4445d286b19568e2d5c08022f04
BuZZy1337 <6979405+BuZZy1337@users.noreply.github.com>
2018-05-01 10:01:49 +0200

    (BuZZy1337) show current Tab in Page-Title

--- src/js/admin.js
+++ src/js/admin.js
@@ -1587,7 +1587,7 @@ $(document).ready(function () {
                 } else {
                     $('#host-adapters-btn').css('opacity', 0.3);
                 }
-                document.title = 'ioBroker.' + tab;
+                document.title = tab + ' - ioBroker';
                 // if some dialog opened or must be shown
                 if (main.currentDialog !== dialog) {
                     // destroy it
