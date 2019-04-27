e873ab5adb0bd6fe2b3fa5e266a79edebfaa1c52
ldittmar <ldittmar.privat@gmail.com>
2019-04-27 20:07:33 +0200

    Remove callback

--- src/js/adminInfo.js
+++ src/js/adminInfo.js
@@ -145,22 +145,14 @@ function InfoAdapter(main) {
                 // if installed version too old
                 that.main.confirmMessage(_('<p>Your version of the info adapter is outdated. It is strongly recommended to update this to get on the enjoyment of the innovations. Only with the new version, for example, messages from the ioBroker team can be displayed directly.</p><p>Would you like to update the info adapter?</p>'), _('Update info adapter'), 'info', function (result) {
                     if (result) {
-                        that.main.cmdExec(null, 'upgrade info', function (exitCode) {
-                            if (!exitCode) {
-                                that._postInit(true);
-                            }
-                        });
+                        that.main.cmdExec(null, 'upgrade info');
                     }
                 });
             } else {
                 // if info adapter is not installed
                 that.main.confirmMessage(_('<p>You have not installed an Info Adapter. The adapter shows you information about the system and is required to display important messages from the ioBroker team.</p><p>Do you want to install the info adapter?</p>'), _('Info adapter not found'), 'info', function (result) {
                     if (result) {
-                        that.main.cmdExec(null, 'add info 0', function (exitCode) {
-                            if (!exitCode) {
-                                that._postInit(true);
-                            }
-                        });
+                        that.main.cmdExec(null, 'add info 0');
                     }
                 });
             }
