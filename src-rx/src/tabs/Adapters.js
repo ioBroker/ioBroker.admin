8f79695bbcbc6f167c904120d4d41c2d844225f5
WLAN-Kabel <wlan-kabel@outlook.de>
2020-03-09 21:02:01 +0100

    Added Adapters

---
+++ src-rx/src/tabs/Adapters.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+
+};
+
+class Adapters extends React.Component {
+
+    render() {
+
+        return(
+            <div>
+                Adapters
+            </div>
+        );
+    }
+
+}
+
+export default withStyles(styles)(Adapters);
