a64f5e7f14dde4219d398a89387c65099c413d5f
WLAN-Kabel <wlan-kabel@outlook.de>
2020-04-30 18:57:13 +0200

    Added ConfirmDialog

---
+++ src-rx/src/components/ConfirmDialog.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import { withStyles } from '@material-ui/core/styles';
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+
+import CloseIcon from '@material-ui/icons/Close';
+
+const styles = theme => ({
+    closeButton: {
+        position: 'absolute',
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.grey[500],
+    }
+});
+
+function ConfirmDialog(props) {
+    return (
+        <Dialog onClose={ props.onClose } open={ props.open }>
+            <DialogTitle>
+                { props.header || '' }
+                <IconButton className={ props.classes.closeButton } onClick={ props.onClose }>
+                    <CloseIcon />
+                </IconButton>
+            </DialogTitle>
+            <DialogContent dividers>
+                <Typography gutterBottom>
+                    { props.children }
+                </Typography>
+            </DialogContent>
+            <DialogActions>
+                <Button autoFocus onClick={ props.onConfirm } color="primary">
+                    { props.confirmText || 'OK' }
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+ConfirmDialog.propTypes = {
+    confirmText: PropTypes.string,
+    header: PropTypes.string,
+    onClose: PropTypes.func.isRequired,
+    onConfirm: PropTypes.func.isRequired,
+    open: PropTypes.bool.isRequired
+};
+
+export default withStyles(styles)(ConfirmDialog);
