73191efb640a4f49fc330a2654916df8c7acff3e
GermanBluefox <dogafox@gmail.com>
2020-07-25 13:45:34 +0200

    Merge changes from Admin5 branch

--- src/js/admin.js
+++ src/js/admin.js
@@ -1697,6 +1697,7 @@ $(document).ready(function () {
         'tab-fullcalendar-0':   {order: 65,   icon: 'perm_contact_calendar'},
         'tab-fullcalendar-1':   {order: 66,   icon: 'perm_contact_calendar'},
         'tab-fullcalendar-2':   {order: 67,   icon: 'perm_contact_calendar'},
+        'tab-echarts':          {order: 70,   icon: 'show_chart'},
         'tab-hosts':            {order: 100,  icon: 'storage'},
     };
 
