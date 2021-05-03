813ae097d8a9e4a11d625d834c48b621c27d731b
igor <zalash1993@gmail.com>
2021-05-03 18:00:11 +0300

    discovery

--- src-rx/src/dialogs/DiscoveryDialog.js
+++ src-rx/src/dialogs/DiscoveryDialog.js
@@ -6,7 +6,7 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import { Tooltip, AppBar, Avatar, Box, Checkbox, CircularProgress, LinearProgress, makeStyles, Paper, Step, StepLabel, Stepper, Switch, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel, ThemeProvider, Typography, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
+import { Tooltip, AppBar, Avatar, Box, Checkbox, CircularProgress, LinearProgress, makeStyles, Paper, Step, StepLabel, Stepper, Switch, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel, ThemeProvider, Typography, Select, MenuItem } from '@material-ui/core';
 
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
