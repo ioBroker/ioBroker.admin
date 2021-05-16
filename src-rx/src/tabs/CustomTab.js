8bb3ba374194af63ddfe0cc240e00990a838988a
simatec <39792461+simatec@users.noreply.github.com>
2021-05-16 18:34:35 +0200

    (simatec) edit border for customTab

--- src-rx/src/tabs/CustomTab.js
+++ src-rx/src/tabs/CustomTab.js
@@ -17,8 +17,8 @@ const styles = theme => ({
         height:     '100%',
         background: 'white',
         color:      'black',
-        borderRadius: 3,
-        border:     '1px solid #888'
+        borderRadius: 4,
+        box-shadow: 0 2px 2px 0 rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 12%), 0 1px 5px 0 rgb(0 0 0 / 20%)
     },
 });
 
