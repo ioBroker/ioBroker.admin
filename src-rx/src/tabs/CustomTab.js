6c79ec8fc00a9c1dcf1eaf7852bd6b4bfaf725b0
simatec <39792461+simatec@users.noreply.github.com>
2021-05-18 09:32:58 +0200

    edit Border and Border-Shadow to admin5 default

--- src-rx/src/tabs/CustomTab.js
+++ src-rx/src/tabs/CustomTab.js
@@ -17,8 +17,9 @@ const styles = theme => ({
         height:     '100%',
         background: 'white',
         color:      'black',
-        borderRadius: 4,
-        boxShadow: '0 2px 2px 0 rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 12%), 0 1px 5px 0 rgb(0 0 0 / 20%)',
+        border-radius: 4,
+        box-shadow: '0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)',
+        border: '0px solid #888'
     },
 });
 
