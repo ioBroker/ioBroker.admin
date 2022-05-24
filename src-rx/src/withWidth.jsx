9a39fc522acf5a577755a027c706d6496b1b45d6
dilesoft <dilesoft@gmail.com>
2022-05-24 12:04:32 +0300

    width fix

--- src-rx/src/withWidth.jsx
+++ src-rx/src/withWidth.jsx
@@ -1,3 +1,22 @@
-const withWidth = () => (WrappedComponent) => (props) => <WrappedComponent {...props} width="xs" />;
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+function useWidth() {
+    const theme = useTheme();
+    const keys = [...theme.breakpoints.keys].reverse();
+    return (
+      keys.reduce((output, key) => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const matches = useMediaQuery(theme.breakpoints.up(key));
+        return !output && matches ? key : output;
+      }, null) || 'xs'
+    );
+  }
+
+// FIXME checkout https://mui.com/components/use-media-query/#migrating-from-withwidth
+const withWidth = () => (WrappedComponent) => (props) => {
+    const width = useWidth();
+    return <WrappedComponent {...props} width={width} />
+};
 
 export default withWidth;
