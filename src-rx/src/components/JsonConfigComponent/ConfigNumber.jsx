47aea3240aa6864ec21dbdc3103f87ad4de92fed
GermanBluefox <dogafox@gmail.com>
2022-06-07 16:46:16 +0200

    Fixed https://github.com/ioBroker/ioBroker.admin/issues/1503

--- src-rx/src/components/JsonConfigComponent/ConfigNumber.jsx
+++ src-rx/src/components/JsonConfigComponent/ConfigNumber.jsx
@@ -2,30 +2,56 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@mui/styles';
 
-import { Autocomplete, TextField } from '@mui/material';
+import { Autocomplete, TextField, InputAdornment, FormControl } from '@mui/material';
 
 import I18n from '@iobroker/adapter-react-v5/i18n';
+import Utils from '@iobroker/adapter-react-v5/Components/Utils';
 
 import ConfigGeneric from './ConfigGeneric';
 
 const styles = theme => ({
     indeterminate: {
         opacity: 0.5
+    },
+    control: {
+      flexDirection: 'row',
+    },
+    textWithArrows: {
+        width: 'calc(100% - 12px)',
+    },
+    arrows: {
+        width: 12,
+        display: 'inline'
+    },
+    arrowUp: {
+        fontSize: 10,
+        userSelect: 'none',
+        cursor: 'pointer',
+    },
+    arrowDown: {
+        fontSize: 10,
+        userSelect: 'none',
+        cursor: 'pointer',
     }
 });
 
 class ConfigNumber extends ConfigGeneric {
     componentDidMount() {
         super.componentDidMount();
-        const value = ConfigGeneric.getValue(this.props.data, this.props.attr);
-        this.setState({value});
+        const _value = ConfigGeneric.getValue(this.props.data, this.props.attr);
+        this.setState({ _value });
         //this.props.registerOnForceUpdate(this.props.attr, this.onUpdate);
     }
 
     static getDerivedStateFromProps(props, state) {
-        const value = ConfigGeneric.getValue(props.data, props.attr);
-        if (value === null || value === undefined || value.toString() !== parseFloat(state.value).toString()) {
-            return {value};
+        if ((props.schema.min !== undefined && props.schema.min < 0) ||
+            (props.schema.max !== undefined && props.schema.max < 0)
+        ) {
+            return null;
+        }
+        const _value = ConfigGeneric.getValue(props.data, props.attr);
+        if (_value === null || _value === undefined || _value.toString() !== parseFloat(state._value).toString()) {
+            return { _value };
         } else {
             return null;
         }
@@ -58,38 +84,85 @@ class ConfigNumber extends ConfigGeneric {
                 />}
             />;
         } else {
-            if (!error && this.state.value !== null && this.state.value !== undefined) {
-                if (this.props.schema.min !== undefined && this.state.value < this.props.schema.min) {
+            if (!error && this.state._value !== null && this.state._value !== undefined) {
+                if (this.props.schema.min !== undefined && this.state._value < this.props.schema.min) {
                     error = I18n.t('Too small');
                 }
-                if (this.props.schema.max !== undefined && this.state.value > this.props.schema.max) {
+                if (this.props.schema.max !== undefined && this.state._value > this.props.schema.max) {
                     error = I18n.t('Too big');
                 }
             }
 
-            return <TextField
+            let type = 'number';
+            if ((this.props.schema.min !== undefined && this.props.schema.min < 0) ||
+                (this.props.schema.max !== undefined && this.props.schema.max < 0)
+            ) {
+                type = 'text';
+            }
+
+            const isNumber = this.state._value !== null &&
+                this.state._value !== undefined &&
+                (typeof this.state._value === 'number' || this.state._value.trim() === parseFloat(this.state._value).toString());
+
+            return <FormControl variant="standard" className={this.props.classes.control}>
+                <TextField
                 variant="standard"
-                type="number"
+                type={type}
                 fullWidth
-                inputProps={{min: this.props.schema.min, max: this.props.schema.max, step: this.props.schema.step}}
-                value={this.state.value === null || this.state.value === undefined ? '' : this.state.value}
+                inputProps={{ min: this.props.schema.min, max: this.props.schema.max, step: this.props.schema.step }}
+                value={this.state._value === null || this.state._value === undefined ? '' : this.state._value}
                 error={!!error}
                 disabled={!!disabled}
+                className={Utils.clsx(type === 'text' && isNumber && this.props.classes.textWithArrows)}
                 onChange={e => {
-                    const value = e.target.value;
-                    if (this.props.schema.min !== undefined && value < this.props.schema.min) {
-                        this.onError(this.props.attr, I18n.t('Too small'));
-                    } else if (this.props.schema.max !== undefined && value > this.props.schema.max) {
-                        this.onError(this.props.attr, I18n.t('Too big'));
+                    const _value = e.target.value;
+                    if (isFinite(_value)) {
+                        if (this.props.schema.min !== undefined && parseFloat(_value) < this.props.schema.min) {
+                            this.onError(this.props.attr, I18n.t('Too small'));
+                        } else if (this.props.schema.max !== undefined && parseFloat(_value) > this.props.schema.max) {
+                            this.onError(this.props.attr, I18n.t('Too big'));
+                        } else if (_value === '-' || isNaN(parseFloat(_value))) {
+                            this.onError(this.props.attr, I18n.t('Not a number'));
+                        } else {
+                            this.onError(this.props.attr); // clear error
+                        }
+                    } else if (_value !== '') {
+                        this.onError(this.props.attr, I18n.t('Not a number'));
                     } else {
                         this.onError(this.props.attr); // clear error
                     }
-                    this.setState({value}, () => this.onChange(this.props.attr, parseFloat(value) || 0));
+
+                    this.setState({ _value }, () => {
+                        if (_value.trim() === parseFloat(_value).toString()) {
+                            this.onChange(this.props.attr, parseFloat(_value) || 0);
+                        }
+                    });
                 }}
                 placeholder={this.getText(this.props.schema.placeholder)}
                 label={this.getText(this.props.schema.label)}
                 helperText={error && typeof error === 'string' ? error : this.renderHelp(this.props.schema.help, this.props.schema.helpLink, this.props.schema.noTranslation)}
-            />;
+                />
+                {type === 'text' && isNumber ? <div className={this.props.classes.arrows}>
+                    <div
+                        className={this.props.classes.arrowUp}
+                        onClick={() => {
+                            let _value = parseFloat(this.state._value) + 1;
+                            if (this.props.schema.max !== undefined && _value <= this.props.schema.max) {
+                                this.setState({ _value });
+                            }
+                        }}
+                    >▲</div>
+                    <div
+                        className={this.props.classes.arrowDown}
+                        onClick={() => {
+                            let _value = parseFloat(this.state._value) - 1;
+                            if (this.props.schema.min !== undefined && _value >= this.props.schema.min) {
+                                this.setState({ _value });
+                            }
+                        }}
+                    >▼</div>
+                </div> : null}
+            </FormControl>;
         }
     }
 }
