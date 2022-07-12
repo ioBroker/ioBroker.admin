1973b9df22a16e263125a640394820ade80382ec
GermanBluefox <dogafox@gmail.com>
2022-07-12 18:31:56 +0200

    https://github.com/ioBroker/ioBroker.admin/issues/1576

--- src-rx/src/tabs/Instances.jsx
+++ src-rx/src/tabs/Instances.jsx
@@ -448,7 +448,11 @@ class Instances extends Component {
             instance.loglevel = common.loglevel || null;
             instance.adapter = common.name || null;
             instance.version = common.version || null;
-            instance.stoppedWhenWebExtension = instance.obj.common.mode === 'daemon' && instance.obj.common.webExtension;
+            instance.stoppedWhenWebExtension = instance.obj.common.mode === 'daemon' ? instance.obj.common.webExtension : undefined;
+
+            if (instance.adapter === 'openweathermap') {
+                debugger;
+            }
 
             if (instance.stoppedWhenWebExtension) {
                 const eId = this.states[instance.id + '.info.extension'] || (await this.props.socket.getState(instance.id + '.info.extension'));
