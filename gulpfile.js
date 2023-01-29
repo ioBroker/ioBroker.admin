8c64eb0f6a3aa9e6ee33175be231b9a42fb02018
GermanBluefox <dogafox@gmail.com>
2023-01-29 21:25:23 +0100

    Rename src-rx to src

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,34 @@
-'use strict';
-
 const fs         = require('fs');
 const cp         = require('child_process');
 const gulp       = require('gulp');
-const del        = require('del');
 const replace    = require('gulp-replace');
 const sourcemaps = require('gulp-sourcemaps');
 const less       = require('gulp-less');
 const concat     = require('gulp-concat');
 const cleanCSS   = require('gulp-clean-css');
 
-const srcRx = 'src-rx/';
-const src = __dirname + '/' + srcRx;
-const dir = src + '/src/i18n/';
+const srcRx = 'src/';
+const src = `${__dirname}/${srcRx}`;
 const dest = 'www/';
+
+function deleteFoldersRecursive(path) {
+    if (path.endsWith('/')) {
+        path = path.substring(0, path.length - 1);
+    }
+    if (fs.existsSync(path)) {
+        const files = fs.readdirSync(path);
+        for (const file of files) {
+            const curPath = `${path}/${file}`;
+            const stat = fs.statSync(curPath);
+            if (stat.isDirectory()) {
+                deleteFoldersRecursive(curPath);
+                fs.rmdirSync(curPath);
+            } else {
+                fs.unlinkSync(curPath);
+            }
+        }
+    }
+}
 
 function npmInstall() {
     return new Promise((resolve, reject) => {
@@ -33,7 +48,7 @@ function npmInstall() {
         child.on('exit', (code /* , signal */) => {
             // code 1 is strange error that cannot be explained. Everything is installed but error :(
             if (code && code !== 1) {
-                reject('Cannot install: ' + code);
+                reject(`Cannot install: ${code}`);
             } else {
                 console.log(`"${cmd} in ${cwd} finished.`);
                 // command succeeded
@@ -44,18 +59,18 @@ function npmInstall() {
 }
 
 function build() {
-    fs.writeFileSync(src + 'public/lib/js/sparkline.js',     fs.readFileSync(`${src}node_modules/@fnando/sparkline/dist/sparkline.js`));
-    fs.writeFileSync(src + 'public/lib/js/sparkline.js.map', fs.readFileSync(`${src}node_modules/@fnando/sparkline/dist/sparkline.js.map`));
+    fs.writeFileSync(`${src}public/lib/js/sparkline.js`,     fs.readFileSync(`${src}node_modules/@fnando/sparkline/dist/sparkline.js`));
+    fs.writeFileSync(`${src}public/lib/js/sparkline.js.map`, fs.readFileSync(`${src}node_modules/@fnando/sparkline/dist/sparkline.js.map`));
 
-    let ace = `${__dirname}/src-rx/node_modules/ace-builds/src-min-noconflict/`;
-    fs.writeFileSync(`${__dirname}/src-rx/public/lib/js/ace/worker-json.js`, fs.readFileSync(`${ace}worker-json.js`));
+    let ace = `${__dirname}/${srcRx}node_modules/ace-builds/src-min-noconflict/`;
+    fs.writeFileSync(`${__dirname}/${srcRx}public/lib/js/ace/worker-json.js`, fs.readFileSync(`${ace}worker-json.js`));
 
     const version = JSON.parse(fs.readFileSync(`${__dirname}/package.json`).toString('utf8')).version;
     const data    = JSON.parse(fs.readFileSync(`${src}package.json`).toString('utf8'));
 
     data.version = version;
 
-    fs.writeFileSync(src + 'package.json', JSON.stringify(data, null, 4));
+    fs.writeFileSync(`${src}package.json`, JSON.stringify(data, null, 4));
 
     return new Promise((resolve, reject) => {
         const options = {
@@ -104,62 +119,61 @@ function build() {
 }
 
 function copyFiles() {
-    return del([
-        dest + '**/*',
-        'admin/custom/**/*',
-        srcRx + 'public/lib/js/crypto-js/*',
-    ])
-        .then(() => Promise.all([
-            gulp.src([
-                srcRx + 'build/**/*',
-                `!${srcRx}build/index.html`,
-                `!${srcRx}build/static/js/*.js`,
-                `!${srcRx}build/i18n/**/*`,
-                `!${srcRx}build/i18n`
-            ])
-                .pipe(gulp.dest(dest)),
+    deleteFoldersRecursive(`${__dirname}/dest`);
+    deleteFoldersRecursive(`${__dirname}/admin/custom`);
+    deleteFoldersRecursive(`${__dirname}/${srcRx}public/lib/js/crypto-js`);
 
-            gulp.src([
-                `${srcRx}build/index.html`,
-            ])
-                .pipe(replace('href="/', 'href="'))
-                .pipe(replace('src="/', 'src="'))
-                .pipe(gulp.dest(dest)),
+    return Promise.all([
+        gulp.src([
+            `${srcRx}build/**/*`,
+            `!${srcRx}build/index.html`,
+            `!${srcRx}build/static/js/*.js`,
+            `!${srcRx}build/i18n/**/*`,
+            `!${srcRx}build/i18n`
+        ])
+            .pipe(gulp.dest(dest)),
 
-            // copy custom plugin
-            gulp.src([`${srcRx}node_modules/@iobroker/admin-component-easy-access/admin/**/*`,])
-                .pipe(gulp.dest('admin/')),
+        gulp.src([
+            `${srcRx}build/index.html`,
+        ])
+            .pipe(replace('href="/', 'href="'))
+            .pipe(replace('src="/', 'src="'))
+            .pipe(gulp.dest(dest)),
 
-            // copy crypto-js
-            gulp.src([`${srcRx}node_modules/crypto-js/*.*`, `!${srcRx}node_modules/crypto-js/CONTRIBUTING.md`, `!${srcRx}node_modules/crypto-js/README.md`])
-                 .pipe(gulp.dest(dest + 'lib/js/crypto-js')),
+        // copy custom plugin
+        gulp.src([`${srcRx}node_modules/@iobroker/admin-component-easy-access/admin/**/*`,])
+            .pipe(gulp.dest('admin/')),
 
-            gulp.src([
-                `${srcRx}build/static/js/*.js`,
-            ])
-                .pipe(replace('s.p+"static/media', '"./static/media'))
-                .pipe(gulp.dest(dest + 'static/js/')),
-        ]));
+        // copy crypto-js
+        gulp.src([`${srcRx}node_modules/crypto-js/*.*`, `!${srcRx}node_modules/crypto-js/CONTRIBUTING.md`, `!${srcRx}node_modules/crypto-js/README.md`])
+             .pipe(gulp.dest(dest + 'lib/js/crypto-js')),
+
+        gulp.src([
+            `${srcRx}build/static/js/*.js`,
+        ])
+            .pipe(replace('s.p+"static/media', '"./static/media'))
+            .pipe(gulp.dest(`${dest}static/js/`)),
+    ]);
 }
 
 function patchIndex() {
     return new Promise(resolve => {
-        if (fs.existsSync(dest + '/index.html')) {
-            let code = fs.readFileSync(dest + '/index.html').toString('utf8');
+        if (fs.existsSync(`${dest}/index.html`)) {
+            let code = fs.readFileSync(`${dest}/index.html`).toString('utf8');
             // replace code
             code = code.replace(/<script>const script=document[^<]+<\/script>/, `<script type="text/javascript" onerror="setTimeout(function(){window.location.reload()}, 5000)" src="./lib/js/socket.io.js"></script>`);
             code = code.replace(/<script>var script=document[^<]+<\/script>/, `<script type="text/javascript" onerror="setTimeout(function(){window.location.reload()}, 5000)" src="./lib/js/socket.io.js"></script>`);
-            fs.writeFileSync(dest + '/index.html', code);
+            fs.writeFileSync(`${dest}/index.html`, code);
             resolve();
         } else {
             // wait till finished
             setTimeout(() => {
-                if (fs.existsSync(dest + '/index.html')) {
-                    let code = fs.readFileSync(dest + '/index.html').toString('utf8');
+                if (fs.existsSync(`${dest}/index.html`)) {
+                    let code = fs.readFileSync(`${dest}/index.html`).toString('utf8');
                     // replace code
                     code = code.replace(/<script>const script=document[^<]+<\/script>/, `<script type="text/javascript" onerror="setTimeout(function(){window.location.reload()}, 5000)" src="./lib/js/socket.io.js"></script>`);
                     code = code.replace(/<script>var script=document[^<]+<\/script>/, `<script type="text/javascript" onerror="setTimeout(function(){window.location.reload()}, 5000)" src="./lib/js/socket.io.js"></script>`);
-                    fs.writeFileSync(dest + '/index.html', code);
+                    fs.writeFileSync(`${dest}/index.html`, code);
                 }
                 resolve();
             }, 2000);
@@ -167,77 +181,11 @@ function patchIndex() {
     });
 }
 
-function i18n2flat() {
-    const files = fs.readdirSync(dir).filter(name => name.match(/\.json$/));
-    const index = {};
-    const langs = [];
-    files.forEach(file => {
-        const lang = file.replace(/\.json$/, '');
-        langs.push(lang);
-        const text = require(dir + file);
-
-        for (const id in text) {
-            if (text.hasOwnProperty(id)) {
-                index[id] = index[id] || {};
-                index[id][lang] = text[id] === undefined ? id : text[id];
-            }
-        }
-    });
-
-    const keys = Object.keys(index);
-    keys.sort();
-
-    if (!fs.existsSync(dir + '/flat/')) {
-        fs.mkdirSync(dir + '/flat/');
-    }
-
-    langs.forEach(lang => {
-        const words = [];
-        keys.forEach(key => {
-            words.push(index[key][lang]);
-        });
-        fs.writeFileSync(`${dir}/flat/${lang}.txt`, words.join('\n'));
-    });
-    fs.writeFileSync(dir + '/flat/index.txt', keys.join('\n'));
-}
-
-function flat2i18n() {
-    if (!fs.existsSync(dir + '/flat/')) {
-        console.error(dir + '/flat/ directory not found');
-        return done();
-    }
-    const keys = fs.readFileSync(dir + '/flat/index.txt').toString().split(/[\r\n]/);
-    while (!keys[keys.length - 1]) keys.splice(keys.length - 1, 1);
-
-    const files = fs.readdirSync(dir + '/flat/').filter(name => name.match(/\.txt$/) && name !== 'index.txt');
-    const index = {};
-    const langs = [];
-    files.forEach(file => {
-        const lang = file.replace(/\.txt$/, '');
-        langs.push(lang);
-        const lines = fs.readFileSync(dir + '/flat/' + file).toString().split(/[\r\n]/);
-        lines.forEach((word, i) => {
-            index[keys[i]] = index[keys[i]] || {};
-            index[keys[i]][lang] = word;
-        });
-    });
-    langs.forEach(lang => {
-        const words = {};
-        keys.forEach((key, line) => {
-            if (!index[key]) {
-                console.log(`No word ${key}, ${lang}, line: ${line}`);
-            }
-            words[key] = index[key][lang];
-        });
-        fs.writeFileSync(`${dir}/${lang}.json`, JSON.stringify(words, null, 4));
-    });
-}
-
 gulp.task('react-0-configCSS', () => {
     return gulp.src([
-        './src-rx/less/selectID.less',
-        './src-rx/less/adapter.less',
-        './src-rx/less/materializeCorrect.less'
+        `./${srcRx}less/selectID.less`,
+        `./${srcRx}less/adapter.less`,
+        `./${srcRx}less/materializeCorrect.less`
     ])
         .pipe(sourcemaps.init())
         .pipe(less({
@@ -246,54 +194,38 @@ gulp.task('react-0-configCSS', () => {
         .pipe(concat('adapter.css'))
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./src-rx/public/css'));
+        .pipe(gulp.dest(`./${srcRx}public/css`));
 });
 
 gulp.task('react-0-iobCSS', () => {
-    return gulp.src(['./src-rx/less/selectID.less'])
+    return gulp.src([`./${srcRx}less/selectID.less`])
         .pipe(sourcemaps.init())
         .pipe(less({
             paths: [ ]
         }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./src-rx/public/lib/css/iob'));
+        .pipe(gulp.dest(`./${srcRx}public/lib/css/iob`));
 });
 
 gulp.task('react-0-treeTableCSS', () => {
-    return gulp.src(['./src-rx/less/jquery.treetable.theme.less'])
+    return gulp.src([`./${srcRx}less/jquery.treetable.theme.less`])
         .pipe(sourcemaps.init())
         .pipe(less({
             paths: [ ]
         }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./src-rx/public/lib/css'));
+        .pipe(gulp.dest(`./${srcRx}public/lib/css`));
 });
 
-gulp.task('react-i18n=>react-flat', done => {
-    i18n2flat();
+gulp.task('react-1-clean', done => {
+    deleteFoldersRecursive(`${__dirname}/${dest}`);
+    deleteFoldersRecursive(`${__dirname}/${srcRx}/build`);
+    deleteFoldersRecursive(`${__dirname}/src/build`);
     done();
-});
-
-gulp.task('react-flat=>react-i18n', done => {
-    flat2i18n();
-    done();
-});
-
-gulp.task('react-1-clean', () => {
-    return del([
-        // 'src/node_modules/**/*',
-        dest + '**/*',
-        dest + '*',
-        srcRx + 'build/**/*'
-    ]).then(del([
-        // 'src/node_modules',
-        'src/build',
-        dest
-    ]));
 });
 
 gulp.task('react-2-npm', () => {
-    if (fs.existsSync(src + 'node_modules')) {
+    if (fs.existsSync(`${src}node_modules`)) {
         return Promise.resolve();
     } else {
         return npmInstall();
