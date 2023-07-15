466bea428e8558fb0f4c659ee4d359f479099240
Max Hauser <moritz.heusinger@gmail.com>
2023-07-15 10:45:20 +0200

    fixed issue with jsonConfig CRON placeholder overlapping input (#1987)
    - closes #1704

--- src/src/components/JsonConfigComponent/ConfigCRON.jsx
+++ src/src/components/JsonConfigComponent/ConfigCRON.jsx
@@ -13,16 +13,16 @@ import ConfigGeneric from './ConfigGeneric';
 
 const styles = theme => ({
     fullWidth: {
-        width: '100%'
+        width: '100%',
     },
     flex: {
-        display: 'flex'
+        display: 'flex',
     },
     button: {
         height: 48,
         marginLeft: 4,
         minWidth: 48,
-    }
+    },
 });
 
 class ConfigCRON extends ConfigGeneric {
@@ -30,53 +30,56 @@ class ConfigCRON extends ConfigGeneric {
         super.componentDidMount();
         const { data, attr } = this.props;
         const value = ConfigGeneric.getValue(data, attr) || '';
-        this.setState({ value});
+        this.setState({ value, showDialog: false });
     }
 
     renderItem(error, disabled, defaultValue) {
         const { classes, schema, attr } = this.props;
         const { value, showDialog } = this.state;
 
-        return <FormControl className={classes.fullWidth} variant="standard">
-            {schema.label ? <InputLabel shrink>{this.getText(schema.label)}</InputLabel> : null}
-            <div className={classes.flex}>
-                <TextField
-                    variant="standard"
-                    fullWidth
-                    value={value}
-                    error={!!error}
-                    disabled={disabled}
-                    placeholder={this.getText(schema.placeholder)}
-                    label={this.getText(schema.label)}
-                    helperText={this.renderHelp(schema.help, schema.helpLink, schema.noTranslation)}
-                    onChange={e => {
-                        const value = e.target.value;
-                        this.setState({ value }, () =>
-                            this.onChange(attr, value))
-                    }}
-                />
-                <Button
-                    color="grey"
-                    className={this.props.classes.button}
-                    size="small"
-                    variant="outlined"
-                    onClick={() => this.setState({ showDialog: true })}
-                >...</Button>
-            </div>
-            {showDialog ? <DialogCron
-                title={I18n.t('ra_Define schedule')}
-                simple={schema.simple}
-                complex={schema.complex}
-                cron={value}
-                language={I18n.getLanguage()}
-                onClose={() => this.setState({ showDialog: false })}
-                cancel={I18n.t('ra_Cancel')}
-                ok={I18n.t('ra_Ok')}
-                onOk={value =>
-                    this.setState({ showDialog: false, value }, () =>
-                        this.onChange(attr, value))}
-            /> : null}
-        </FormControl>;
+        return (
+            <FormControl className={classes.fullWidth} variant="standard">
+                {schema.label ? <InputLabel shrink>{this.getText(schema.label)}</InputLabel> : null}
+                <div className={classes.flex}>
+                    <TextField
+                        variant="standard"
+                        fullWidth
+                        value={value ?? ''}
+                        error={!!error}
+                        disabled={disabled}
+                        placeholder={this.getText(schema.placeholder)}
+                        label={this.getText(schema.label)}
+                        helperText={this.renderHelp(schema.help, schema.helpLink, schema.noTranslation)}
+                        onChange={e => {
+                            const value = e.target.value;
+                            this.setState({ value }, () => this.onChange(attr, value));
+                        }}
+                    />
+                    <Button
+                        color="grey"
+                        className={this.props.classes.button}
+                        size="small"
+                        variant="outlined"
+                        onClick={() => this.setState({ showDialog: true })}
+                    >
+                        ...
+                    </Button>
+                </div>
+                {showDialog ? (
+                    <DialogCron
+                        title={I18n.t('ra_Define schedule')}
+                        simple={schema.simple}
+                        complex={schema.complex}
+                        cron={value}
+                        language={I18n.getLanguage()}
+                        onClose={() => this.setState({ showDialog: false })}
+                        cancel={I18n.t('ra_Cancel')}
+                        ok={I18n.t('ra_Ok')}
+                        onOk={value => this.setState({ showDialog: false, value }, () => this.onChange(attr, value))}
+                    />
+                ) : null}
+            </FormControl>
+        );
     }
 }
 
