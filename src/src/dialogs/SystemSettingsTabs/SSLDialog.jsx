261122959521a2af86c94b221e4012475a745788
Max Hauser <moritz.heusinger@gmail.com>
2023-07-18 08:52:29 +0200

    fix css style on system settings ssl tab (#1991)
    - to make link better clickable
    - closes #1949

--- src/src/dialogs/SystemSettingsTabs/SSLDialog.jsx
+++ src/src/dialogs/SystemSettingsTabs/SSLDialog.jsx
@@ -20,8 +20,8 @@ const styles = theme => ({
         //backgroundColor: blueGrey[ 50 ]
     },
     buttonPanel: {
-        paddingBottom: 40,
-        display: 'flex'
+        paddingBottom: 50,
+        display: 'flex',
     },
     descriptionPanel: {
         width: '100%',
@@ -33,8 +33,7 @@ const styles = theme => ({
         '& a': {
             paddingLeft: 3,
             color: theme.palette.mode === 'dark' ? '#EEE' : '#111',
-
-        }
+        },
     },
     formControl: {
         margin: theme.spacing(1),
@@ -44,90 +43,92 @@ const styles = theme => ({
 
 class SSLDialog extends Component {
     render() {
-        const {classes, data} = this.props;
-        const {letsEncrypt} = data.native || {};
-        return <div className={classes.tabPanel}>
-            <div
-                style={{
-                    width: '100%',
-                    // height: '100% ',
-                    overflow: 'auto',
-                    overflowX: 'hidden',
-                    padding: 15,
-                    fontSize: 20,
-                    color: '#ff4949',
-                }}
-            >
-                {this.props.t('ra_Use iobroker.acme adapter for letsencrypt certificates')}
+        const { classes, data } = this.props;
+        const { letsEncrypt } = data.native || {};
+        return (
+            <div className={classes.tabPanel}>
+                <div
+                    style={{
+                        width: '100%',
+                        // height: '100% ',
+                        overflow: 'auto',
+                        overflowX: 'hidden',
+                        padding: 15,
+                        fontSize: 20,
+                        color: '#ff4949',
+                    }}
+                >
+                    {this.props.t('ra_Use iobroker.acme adapter for letsencrypt certificates')}
+                </div>
+                <div className={classes.buttonPanel}>
+                    <Paper
+                        variant="outlined"
+                        className={classes.descriptionPanel}
+                        dangerouslySetInnerHTML={{ __html: this.props.t('letsnecrypt_help') }}
+                    />
+                </div>
+                <Grid container spacing={6}>
+                    <Grid item md={3} xs={12}>
+                        <FormControl variant="standard" className={classes.formControl}>
+                            <TextField
+                                variant="standard"
+                                id="email"
+                                label={this.props.t('Email for account:')}
+                                value={letsEncrypt ? letsEncrypt.email : ''}
+                                InputLabelProps={{
+                                    readOnly: false,
+                                    shrink: true,
+                                }}
+                                onChange={evt => this.onChangeText(evt, 'email')}
+                            />
+                        </FormControl>
+                    </Grid>
+                    <Grid item md={3} xs={12}>
+                        <FormControl variant="standard" className={classes.formControl}>
+                            <TextField
+                                variant="standard"
+                                id="domains"
+                                label={this.props.t('Domains:')}
+                                value={letsEncrypt ? letsEncrypt.domains : ''}
+                                InputLabelProps={{
+                                    readOnly: false,
+                                    shrink: true,
+                                }}
+                                onChange={evt => this.onChangeText(evt, 'domains')}
+                            />
+                        </FormControl>
+                    </Grid>
+                    <Grid item md={3} xs={12}>
+                        <FormControl variant="standard" className={classes.formControl}>
+                            <TextField
+                                variant="standard"
+                                id="path"
+                                label={this.props.t('Path to storage:')}
+                                value={letsEncrypt ? letsEncrypt.path : ''}
+                                InputLabelProps={{
+                                    readOnly: false,
+                                    shrink: true,
+                                }}
+                                onChange={evt => this.onChangeText(evt, 'path')}
+                            />
+                        </FormControl>
+                    </Grid>
+                </Grid>
             </div>
-            <div className={classes.buttonPanel}>
-                <Paper
-                    variant="outlined"
-                    className={classes.descriptionPanel}
-                    dangerouslySetInnerHTML={{__html: this.props.t('letsnecrypt_help')}}
-                />
-            </div>
-            <Grid container spacing={6}>
-                <Grid item md={3} xs={12}>
-                    <FormControl variant="standard" className={classes.formControl}>
-                        <TextField
-                            variant="standard"
-                            id="email"
-                            label={this.props.t('Email for account:')}
-                            value={letsEncrypt ? letsEncrypt.email : ''}
-                            InputLabelProps={{
-                                readOnly: false,
-                                shrink: true,
-                            }}
-                            onChange={evt => this.onChangeText(evt, 'email')}
-                        />
-                    </FormControl>
-                </Grid>
-                <Grid item md={3} xs={12}>
-                    <FormControl variant="standard" className={classes.formControl}>
-                        <TextField
-                            variant="standard"
-                            id="domains"
-                            label={this.props.t('Domains:')}
-                            value={letsEncrypt ? letsEncrypt.domains : ''}
-                            InputLabelProps={{
-                                readOnly: false,
-                                shrink: true,
-                            }}
-                            onChange={evt => this.onChangeText(evt, 'domains')}
-                        />
-                    </FormControl>
-                </Grid>
-                <Grid item md={3} xs={12}>
-                    <FormControl variant="standard" className={classes.formControl}>
-                        <TextField
-                            variant="standard"
-                            id="path"
-                            label={this.props.t('Path to storage:')}
-                            value={letsEncrypt ? letsEncrypt.path : ''}
-                            InputLabelProps={{
-                                readOnly: false,
-                                shrink: true,
-                            }}
-                            onChange={evt => this.onChangeText(evt, 'path')}
-                        />
-                    </FormControl>
-                </Grid>
-            </Grid>
-        </div>;
+        );
     }
 
     onChangeText = (evt, id) => {
         const value = evt.target.value;
         this.doChange(id, value);
-    }
+    };
 
     doChange = (name, value) => {
-        let newData = JSON.parse(JSON.stringify(this.props.data))
+        let newData = JSON.parse(JSON.stringify(this.props.data));
         newData.native.letsEncrypt = newData.native.letsEncrypt || {};
         newData.native.letsEncrypt[name] = value;
         this.props.onChange(newData);
-    }
+    };
 }
 
 SSLDialog.propTypes = {
@@ -137,6 +138,3 @@ SSLDialog.propTypes = {
 };
 
 export default withWidth()(withStyles(styles)(SSLDialog));
-
-
-
