e330998bc6aa326addb5a92a2b57d9c3710f6715
foxriver76 <moritz.heusinger@gmail.com>
2023-08-17 09:05:17 +0200

    started porting admin backend to es6
    - and modernize code

---
+++ lib/adapter-config.d.ts
@@ -0,0 +1,18 @@
+// This file extends the AdapterConfig type from "@types/iobroker"
+// using the actual properties present in io-package.json
+// in order to provide typings for adapter.config properties
+import { native } from '../io-package.json';
+
+type _AdapterConfig = typeof native;
+
+// Augment the globally declared type ioBroker.AdapterConfig
+declare global {
+    namespace ioBroker {
+        interface AdapterConfig extends _AdapterConfig {
+            // Do not enter anything here!
+        }
+    }
+}
+
+// this is required so the above AdapterConfig is found by TypeScript / type checking
+export {};
