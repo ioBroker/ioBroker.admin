e040191b83f412b63ce4765fcc397c561af505dd
Max Hauser <moritz.heusinger@gmail.com>
2023-12-20 12:05:28 +0100

    export types too from jsconfig module (#2271)

--- packages/jsonConfig/src/index.tsx
+++ packages/jsonConfig/src/index.tsx
@@ -3,6 +3,8 @@ import JsonConfigComponent from './JsonConfigComponent/index';
 import ConfigPanel from './JsonConfigComponent/ConfigPanel';
 import ConfigGeneric from './JsonConfigComponent/ConfigGeneric';
 
+export type * from './JsonConfigComponent/ConfigGeneric';
+
 export {
     JsonConfig,
     JsonConfigComponent,
