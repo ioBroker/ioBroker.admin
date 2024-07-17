f07e1e12e70bf6e9fe57fccee1ec239d9776c244
foxriver76 <moritz.heusinger@gmail.com>
2024-07-17 14:51:41 +0200

    rm form control

--- packages/jsonConfig/src/JsonConfigComponent/ConfigQrCode.tsx
+++ packages/jsonConfig/src/JsonConfigComponent/ConfigQrCode.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { FormControl } from '@mui/material';
 import QRCode from 'react-qr-code';
 
 import type { ConfigItemQrCode } from '#JC/types';
@@ -12,9 +11,7 @@ interface ConfigQrCodeProps extends ConfigGenericProps {
 
 class ConfigQrCode extends ConfigGeneric<ConfigQrCodeProps, ConfigGenericState> {
     renderItem() {
-        return <FormControl variant="standard">
-            <QRCode value={this.props.schema.data} size={this.props.schema.size} fgColor={this.props.schema.fgColor} bgColor={this.props.schema.bgColor} level={this.props.schema.level} />
-        </FormControl>;
+        return <QRCode value={this.props.schema.data} size={this.props.schema.size} fgColor={this.props.schema.fgColor} bgColor={this.props.schema.bgColor} level={this.props.schema.level} />;
     }
 }
 
