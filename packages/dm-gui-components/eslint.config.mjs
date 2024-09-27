c88acf322aa8f31971d9f35afbc0162fd9c80420
foxriver76 <moritz.heusinger@gmail.com>
2024-09-27 09:28:34 +0200

    disable rules the recommended way

--- packages/dm-gui-components/eslint.config.mjs
+++ packages/dm-gui-components/eslint.config.mjs
@@ -1,13 +1,5 @@
 import config, { reactConfig } from '@iobroker/eslint-config';
 
-// disable temporary the rule 'jsdoc/require-param' and enable 'jsdoc/require-jsdoc'
-config.forEach(rule => {
-    if (rule?.plugins?.jsdoc) {
-        rule.rules['jsdoc/require-jsdoc'] = 'off';
-        rule.rules['jsdoc/require-param'] = 'off';
-    }
-});
-
 export default [
     ...config,
     ...reactConfig,
@@ -21,4 +13,12 @@ export default [
             },
         },
     },
+    ,
+    {
+        // disable temporary the rule 'jsdoc/require-param' and enable 'jsdoc/require-jsdoc'
+        rules: {
+            'jsdoc/require-jsdoc': 'off',
+            'jsdoc/require-param': 'off'
+        }
+    }
 ];
