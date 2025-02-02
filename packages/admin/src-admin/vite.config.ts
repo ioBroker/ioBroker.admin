4e4eb7ac6b8c1b837e2ff2ad328a9091bca4c078
dilesoft <dilesoft@gmail.com>
2025-02-02 11:46:06 +0300

    vite module

---
+++ packages/admin/src-admin/vite.config.ts
@@ -0,0 +1,60 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import commonjs from 'vite-plugin-commonjs';
+import svgr from 'vite-plugin-svgr';
+import vitetsConfigPaths from 'vite-tsconfig-paths';
+import { federation } from '@module-federation/vite';
+import { shared } from './modulefederation.admin.config';
+import path from 'path';
+
+const makeShared = (pkgs: string[]): Record<string, { requiredVersion: '*', singleton: true }> => {
+    const result: Record<string, { requiredVersion: '*', singleton: true }>= {};
+    pkgs.forEach(packageName => {
+        result[packageName] = {
+            requiredVersion: '*',
+            singleton: true,
+        };
+    });
+    return result;
+};
+
+export default defineConfig({
+    plugins: [
+        federation({
+            name: 'iobroker_admin',
+            shared,
+            exposes: {},
+            remotes: {},
+            filename: 'remoteEntry.js',
+            manifest: true,
+        }),
+        react(),
+        vitetsConfigPaths(),
+        commonjs(),
+    ],
+    server: {
+        port: 3000,
+        proxy: {
+            '/files': 'http://127.0.0.1:8081',
+            '/adapter': 'http://127.0.0.1:8081',
+            '/session': 'http://127.0.0.1:8081',
+            '/log': 'http://127.0.0.1:8081',
+            '/lib/js/crypto-js': 'http://127.0.0.1:8081',
+        },
+    },
+    base: './',
+    resolve: {
+        alias: {
+            '@iobroker/json-config': path.resolve(__dirname, '..', '..', 'jsonConfig', 'src'),
+            '@iobroker/adapter-react-v5': path.resolve(__dirname, '..', '..', 'adapter-react-v5', 'src'),
+            '@iobroker/dm-gui-components': path.resolve(__dirname, '..', '..', 'dm-gui-components', 'src'),
+            '@': path.resolve(__dirname, 'src'),
+            '#DM': path.resolve(__dirname, '..', '..', 'dm-gui-components', 'src'),
+            '#JC': path.resolve(__dirname, '..', '..', 'jsonConfig', 'src'),
+        },
+    },
+    build: {
+        target: 'chrome89',
+        outDir: './build',
+    },
+});
